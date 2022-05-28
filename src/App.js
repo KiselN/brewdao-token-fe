a576a02165ceaf8651949a7831b0dfcb01aff8d5
Nikita Kisel <nikita.kisel@outlook.com>
2022-05-29 01:47:15 +0400

    Added ability to participate in BrewDAO token sale

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { requestAccount, getContract } from './utils/common';
+import { contractAddress } from './config';
+import brewDao from './contracts/ETHBrewDao.json';
+import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+
 function App() {
+  let contract = getContract(contractAddress, brewDao);
+  const [supply, setSupply] = useState();
+  const [price, setPrice] = useState();
+  const [amountToBuy, setAmountToBuy] = useState();
+
+  const getSupply = async () => {
+    await requestAccount();
+    const supply = await contract.totalSupply();
+    const price = await contract.tokenPrice();
+    setSupply(supply.toNumber());
+    setPrice(ethers.utils.formatEther(price));
+  };
+
+  const buyTokens = async () => {
+    await contract.buyOnInitialOffering({ value: ethers.utils.parseEther(amountToBuy.toString()) });
+  };
+
+  useEffect(() => {
+    getSupply();
+  }, []);
+
   return (
     <div className="App">
-      Ethereum Fullstack Template
+      <p>Token price: {price} ETH</p>
+      <p>Token supply: {supply}</p>
+      <p><input type="number" onChange={(e) => { setAmountToBuy(e.target.value) }} /></p>
+      <button onClick={buyTokens}>Buy Token</button>
     </div>
   );
 }
